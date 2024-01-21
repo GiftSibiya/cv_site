3f7d18e4615fbd9b623b7d1454040bc131b691cf
GiftSibiya <sibiya.bobo@gmail.com>
2024-01-22 00:17:27 +0200

    featured in page

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,16 @@ import React from "react";
 ///--///
 
 function HeroSection() {
-  return <section className="Hero-Container"></section>;
+  return (
+    <section className="Hero-Container">
+      <div className="Hero-Tagline"> Why use our writting services</div>
+      <div className="Hero-Main">
+        <div className="Hero-Tiks">Ticks</div>
+        <div className="Hero-Projects"> Projects</div>
+      </div>
+      <div className="Hero-Features"></div>
+    </section>
+  );
 }
 
 export default HeroSection;
